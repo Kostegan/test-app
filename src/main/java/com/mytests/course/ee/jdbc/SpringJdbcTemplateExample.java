067fe974189a098a3fea1516417f4c382924e5aa
Kostegan <churilovkonstantin@gmail.com>
2017-03-02 11:37:58 +0400

    Added new methods for a work with a dataBase

--- src/main/java/com/mytests/course/ee/jdbc/SpringJdbcTemplateExample.java
+++ src/main/java/com/mytests/course/ee/jdbc/SpringJdbcTemplateExample.java
@@ -1,11 +1,21 @@
 package com.mytests.course.ee.jdbc;
 
 
+import com.mytests.course.ee.jdbc.model.Region;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.*;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SpringJdbcTemplateExample {
 
@@ -15,20 +25,59 @@ public class SpringJdbcTemplateExample {
         ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"springContext.xml"});
         JdbcTemplate jdbc = context.getBean("jdbcTemplate", JdbcTemplate.class);
 
-        queryForSimpleObject(jdbc);
+//        queryForSimpleObject(jdbc);
+//        queryForComplexObject(jdbc);
+//        insertNewRegion(jdbc);
+        insertConstructor(jdbc);
+
     }
 
     private static void separator(String title) {
         log.debug("\r\n\r\n\r\n\r\n");
+        log.debug("=========={}==========", title);
+//        log.debug("");
     }
 
     private static void queryForSimpleObject(JdbcTemplate jdbc) {
         separator("queryForSimpleObject");
-        int countOfRegion = jdbc.queryForObject("select count(*) from test ", Integer.class);
-//        int countOfRegion = jdbc.queryForObject("select count(*) from test where region_name=?", new String[]{"Moscow"}, Integer.class);
-        log.debug("Region count:{}",countOfRegion);
+        int countOfRegion = jdbc.queryForObject("select count(*) from js_region where region_name=?", new String[]{"Moscow"}, Integer.class);
+        log.debug("Region count:{}", countOfRegion);
 
-//        String regionName = jdbc.queryForObject("select user_name from test where region_id=? and region_name=?", new Object[]{1, "Moscow"}, String.class);
-//        log.debug("Region name: {}",regionName);
+        String regionName = jdbc.queryForObject("select region_name from js_region where id=? and region_name=?", new Object[]{2, "Moscow"}, String.class);
+        log.debug("Region name: {}", regionName);
+    }
+
+    private static void queryForComplexObject(JdbcTemplate jdbc) {
+        separator("queryForSimpleObject");
+        Region region = jdbc.queryForObject("select id, region_name from js_region where id=? and region_name=?", new Object[]{4, "Voronegskaya Obl"}, new RowMapper<Region>() {
+            @Override
+            public Region mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new Region(rs.getInt("id"), rs.getString("region_name"));
+            }
+        });
+        log.debug("Region = {}", region);
+    }
+
+    private static void insertNewRegion(JdbcTemplate jdbc) {
+        separator("insertNewRegion");
+        PreparedStatementCreatorFactory creatorFactory = new PreparedStatementCreatorFactory("insert into js_region (region_name) values (?)");
+        creatorFactory.setGeneratedKeysColumnNames("id");
+        creatorFactory.addParameter(new SqlParameter(Types.VARCHAR));
+        PreparedStatementCreator preparedStatementCreator = creatorFactory.newPreparedStatementCreator(new String[]{"HMAO"});
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int insertCount = jdbc.update(preparedStatementCreator, keyHolder);
+        log.debug("Insert count: {}", insertCount);
+        log.debug("New Region's id: {}", keyHolder.getKey().intValue());
+    }
+
+    private static void insertConstructor(JdbcTemplate jdbc) {
+        separator("insertConstructor");
+        SimpleJdbcInsert insertActor = new SimpleJdbcInsert(jdbc).withTableName("js_region").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("region_name", "new region name2");
+//        insertActor.execute(parameters);
+        Number number = insertActor.executeAndReturnKey(parameters);
+//        log.debug("Inserted region id: {}", number.longValue());
     }
 }
